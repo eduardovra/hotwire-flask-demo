a4ee87faded11d0cdda3a103ddcd5d83b2dd563e
Eduardo Vieira <eduardovra@gmail.com>
2021-04-24 11:24:14 -0300

    WIP Form validation

--- turbo-form-validation/app.py
+++ turbo-form-validation/app.py
@@ -3,12 +3,38 @@ from turbo_flask import Turbo
 
 app = Flask(__name__)
 turbo = Turbo(app)
+users = []
 
 @app.route("/", methods=["GET",])
 def index():
-    return render_template("index.html")
+    global users
+
+    context = {'username': {}, 'password': {}}
+    return render_template("index.html", users=users, **context)
 
 @app.route("/signup", methods=["POST",])
 def signup():
-    data = request.form
+    global users
+
+    # Validade form
+    data = {field: {'value': value}
+            for field, value in request.form.items()}
+
+    if turbo.can_stream():
+        stream = []
+
+        if data['username'] in users:
+            stream.append(
+                turbo.replace(
+                    
+                )
+            )
+
+    users.insert(0, data)
+
+    # hashed_password = hashlib.sha512(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
+    #return render_template("index.html", users=users, **data)
+
+    # Everything is fine
     return redirect(url_for("index"))
